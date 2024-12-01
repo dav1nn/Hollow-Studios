b760af20fea56d9b545e6f4c5c6ad4bae07d4ba0
Oransarge <C21506079@mytudublin.ie>
2024-12-01 18:13:30 +0000

    keyboard for register

---
+++ Assets/Scripts/KeyPressSound.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyPressSound : MonoBehaviour
+{
+    public TMP_InputField inputField;  
+    public AudioSource typingAudioSource;  
+    public AudioSource deleteAudioSource;  
+
+    private string previousText = ""; 
+
+    void Update()
+    {
+        if (inputField.isFocused)
+        {
+            
+            if (Input.anyKeyDown && !Input.GetKey(KeyCode.Backspace))
+            {
+                PlayTypingSound();
+            }
+
+            
+            if (Input.GetKey(KeyCode.Backspace) && inputField.text.Length < previousText.Length)
+            {
+                PlayDeleteSound();
+            }
+
+            
+            previousText = inputField.text;
+        }
+    }
+
+    void PlayTypingSound()
+    {
+        if (typingAudioSource != null)
+        {
+            typingAudioSource.PlayOneShot(typingAudioSource.clip);
+        }
+    }
+
+    void PlayDeleteSound()
+    {
+        if (deleteAudioSource != null)
+        {
+            deleteAudioSource.PlayOneShot(deleteAudioSource.clip);
+        }
+    }
+}
+
+
+
+
+
