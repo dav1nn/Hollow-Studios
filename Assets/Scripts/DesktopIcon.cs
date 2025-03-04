d8a0cd59ffced525a61b0d567fbd33169555a2db
Oransarge <C21506079@mytudublin.ie>
2025-03-04 20:30:40 +0000

    panel animation

--- Assets/Scripts/DesktopIcon.cs
+++ Assets/Scripts/DesktopIcon.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class DesktopIcon : MonoBehaviour, IPointerClickHandler
 {
@@ -8,6 +9,17 @@ public class DesktopIcon : MonoBehaviour, IPointerClickHandler
 
     [SerializeField] private GameObject filePanel;
     [SerializeField] private ConversationManager conversationManager;
+
+    private Vector3 originalScale;  
+
+    private void Awake()
+    {
+        
+        if (filePanel != null)
+        {
+            originalScale = filePanel.transform.localScale;
+        }
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -27,11 +39,40 @@ public class DesktopIcon : MonoBehaviour, IPointerClickHandler
     {
         if (filePanel != null)
         {
+            
             filePanel.SetActive(true);
+
+            
+            filePanel.transform.localScale = Vector3.zero;
+
+            
+            StartCoroutine(AnimatePanel(filePanel, 0.2f, originalScale));
         }
         else
         {
-            Debug.LogWarning("No file.");
+            Debug.LogWarning("No file panel is assigned.");
         }
     }
+
+    private IEnumerator AnimatePanel(GameObject panel, float duration, Vector3 targetScale)
+    {
+        float timeElapsed = 0f;
+        Vector3 startScale = panel.transform.localScale;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float t = timeElapsed / duration;
+
+            
+            panel.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+
+            yield return null;
+        }
+
+        
+        panel.transform.localScale = targetScale;
+    }
 }
+
+
--- Assets/Scripts/PictureFolder.cs
+++ Assets/Scripts/PictureFolder.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro; 
+using TMPro;
+using System.Collections;
 
 public class PictureFolder : MonoBehaviour
 {
@@ -10,8 +11,15 @@ public class PictureFolder : MonoBehaviour
     [SerializeField] private string[] pictureDescriptions;
     [SerializeField] private GameObject enlargePanel;
     [SerializeField] private Image enlargeImage;
-    [SerializeField] private TextMeshProUGUI enlargeText; 
+    [SerializeField] private TextMeshProUGUI enlargeText;
     [SerializeField] private GameObject folderPanel;
+
+    private Vector3 enlargePanelOriginalScale;
+
+    private void Awake()
+    {
+        if (enlargePanel != null) enlargePanelOriginalScale = enlargePanel.transform.localScale;
+    }
 
     private void OnEnable()
     {
@@ -20,27 +28,18 @@ public class PictureFolder : MonoBehaviour
 
     private void PopulateGrid()
     {
-        foreach (Transform child in pictureGrid)
-        {
-            Destroy(child.gameObject);
-        }
-
+        foreach (Transform child in pictureGrid) Destroy(child.gameObject);
         for (int i = 0; i < pictureSprites.Length; i++)
         {
             Sprite sprite = pictureSprites[i];
             string description = i < pictureDescriptions.Length ? pictureDescriptions[i] : "No description available";
-
             GameObject newPicture = Instantiate(picturePrefab, pictureGrid);
             Image imageComponent = newPicture.GetComponent<Image>();
             if (imageComponent != null)
             {
                 imageComponent.sprite = sprite;
-
                 Button button = newPicture.GetComponent<Button>();
-                if (button != null)
-                {
-                    button.onClick.AddListener(() => EnlargeImage(sprite, description));
-                }
+                if (button != null) button.onClick.AddListener(() => EnlargeImage(sprite, description));
             }
         }
     }
@@ -50,24 +49,35 @@ public class PictureFolder : MonoBehaviour
         if (enlargePanel != null && enlargeImage != null && enlargeText != null)
         {
             enlargeImage.sprite = sprite;
-            enlargeText.text = description; 
+            enlargeText.text = description;
+            enlargePanel.transform.localScale = Vector3.zero;
             enlargePanel.SetActive(true);
+            StartCoroutine(AnimatePanelOpen(enlargePanel, 0.2f, enlargePanelOriginalScale));
         }
+    }
+
+    private IEnumerator AnimatePanelOpen(GameObject panel, float duration, Vector3 targetScale)
+    {
+        float elapsed = 0f;
+        Vector3 startScale = panel.transform.localScale;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            panel.transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            yield return null;
+        }
+        panel.transform.localScale = targetScale;
     }
 
     public void CloseEnlargePanel()
     {
-        if (enlargePanel != null)
-        {
-            enlargePanel.SetActive(false);
-        }
+        if (enlargePanel != null) enlargePanel.SetActive(false);
     }
 
     public void CloseFolder()
     {
-        if (folderPanel != null)
-        {
-            folderPanel.SetActive(false);
-        }
+        if (folderPanel != null) folderPanel.SetActive(false);
     }
 }
+
