e936eb87ae91341c3e3ce874b583d264662270b2
Oransarge <C21506079@mytudublin.ie>
2025-03-12 18:23:33 +0000

    two more, bug fixes

--- Assets/Scripts/ChatPanelSwitcher.cs
+++ Assets/Scripts/ChatPanelSwitcher.cs
@@ -5,27 +5,54 @@ public class ChatPanelSwitcher : MonoBehaviour
 {
     public GameObject momPanel;
     public GameObject dadPanel;
+    public GameObject harryPanel;
+    public GameObject anaPanel;
 
     public Button momButton;
     public Button dadButton;
+    public Button harryButton;
+    public Button anaButton;
 
     void Start()
     {
-        momButton.onClick.AddListener(ActivateMomPanel);
-        dadButton.onClick.AddListener(ActivateDadPanel);
+        momButton.onClick.AddListener(ShowMomPanel);
+        dadButton.onClick.AddListener(ShowDadPanel);
+        harryButton.onClick.AddListener(ShowHarryPanel);
+        anaButton.onClick.AddListener(ShowAnaPanel);
 
-        ActivateMomPanel();
+        ShowMomPanel();
     }
 
-    void ActivateMomPanel()
+    void ShowMomPanel()
     {
         momPanel.SetActive(true);
         dadPanel.SetActive(false);
+        harryPanel.SetActive(false);
+        anaPanel.SetActive(false);
     }
 
-    void ActivateDadPanel()
+    void ShowDadPanel()
     {
-        dadPanel.SetActive(true);
         momPanel.SetActive(false);
+        dadPanel.SetActive(true);
+        harryPanel.SetActive(false);
+        anaPanel.SetActive(false);
+    }
+
+    void ShowHarryPanel()
+    {
+        momPanel.SetActive(false);
+        dadPanel.SetActive(false);
+        harryPanel.SetActive(true);
+        anaPanel.SetActive(false);
+    }
+
+    void ShowAnaPanel()
+    {
+        momPanel.SetActive(false);
+        dadPanel.SetActive(false);
+        harryPanel.SetActive(false);
+        anaPanel.SetActive(true);
     }
 }
+
--- Assets/Scripts/ResetScrollOnEnable.cs
+++ Assets/Scripts/ResetScrollOnEnable.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class ResetScrollOnEnableMulti : MonoBehaviour
+public class ResetScrollOnEnable : MonoBehaviour
 {
-    [SerializeField] private ScrollRect[] scrollRects;
+    [SerializeField] private ScrollRect momScrollRect;
+    [SerializeField] private ScrollRect dadScrollRect;
+    [SerializeField] private ScrollRect harryScrollRect;
+    [SerializeField] private ScrollRect anaScrollRect;
 
     private void OnEnable()
     {
-        StartCoroutine(ResetScroll());
+        StartCoroutine(ResetAllScrolls());
     }
 
-    private IEnumerator ResetScroll()
+    private IEnumerator ResetAllScrolls()
     {
         yield return null;
         Canvas.ForceUpdateCanvases();
-        foreach (var sr in scrollRects)
-        {
-            sr.verticalNormalizedPosition = 1f;
-        }
+
+        if (momScrollRect)    momScrollRect.verticalNormalizedPosition = 1f;
+        if (dadScrollRect)    dadScrollRect.verticalNormalizedPosition = 1f;
+        if (harryScrollRect)  harryScrollRect.verticalNormalizedPosition = 1f;
+        if (anaScrollRect)    anaScrollRect.verticalNormalizedPosition = 1f;
     }
 }
+
 
 
