1929bca039ceb5593960d73cb313f55fa4face17
dav1nn <davinayyubov@gmail.com>
2025-03-17 04:53:12 +0000

    noah sneak
    noah intro done

--- Assets/Scripts/DateCodeCheck.cs
+++ Assets/Scripts/DateCodeCheck.cs
@@ -21,7 +21,6 @@ public class DateCodeCheck : MonoBehaviour
         originalButtonColor = nextButton.GetComponent<Image>().color;
         originalButtonText = nextButton.GetComponentInChildren<TMP_Text>().text;
         nextButton.interactable = false;
-
         dayInput.onValueChanged.AddListener(delegate { ValidateInputs(); });
         monthInput.onValueChanged.AddListener(delegate { ValidateInputs(); });
         yearInput.onValueChanged.AddListener(delegate { ValidateInputs(); });
@@ -38,8 +37,7 @@ public class DateCodeCheck : MonoBehaviour
     {
         if (dayInput.text == "27" && monthInput.text == "08" && yearInput.text == "1985")
         {
-            currentGameObject.SetActive(false);
-            nextGameObject.SetActive(true);
+            StartCoroutine(GlitchAndDeactivate());
         }
         else
         {
@@ -53,12 +51,40 @@ public class DateCodeCheck : MonoBehaviour
         nextButton.GetComponentInChildren<TMP_Text>().text = "ERROR";
         nextButton.GetComponent<Image>().color = Color.red;
         accessDeniedText.gameObject.SetActive(true);
-
         yield return new WaitForSeconds(2);
-
         nextButton.GetComponentInChildren<TMP_Text>().text = originalButtonText;
         nextButton.GetComponent<Image>().color = originalButtonColor;
         accessDeniedText.gameObject.SetActive(false);
         ValidateInputs();
+    }
+
+    IEnumerator GlitchAndDeactivate()
+    {
+        Vector3 originalPos = currentGameObject.transform.localPosition;
+        Vector3 originalScale = currentGameObject.transform.localScale;
+        float glitchDuration = 1f;
+        float elapsed = 0f;
+        while (elapsed < glitchDuration)
+        {
+            float offsetX = Random.Range(-20f, 20f);
+            float offsetY = Random.Range(-20f, 20f);
+            currentGameObject.transform.localPosition = originalPos + new Vector3(offsetX, offsetY, 0f);
+            yield return new WaitForSeconds(0.01f);
+            currentGameObject.transform.localPosition = originalPos;
+            elapsed += 0.01f;
+        }
+        float shrinkDuration = 0.1f;
+        float shrinkElapsed = 0f;
+        while (shrinkElapsed < shrinkDuration)
+        {
+            float t = shrinkElapsed / shrinkDuration;
+            currentGameObject.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
+            shrinkElapsed += Time.deltaTime;
+            yield return null;
+        }
+        currentGameObject.SetActive(false);
+        currentGameObject.transform.localScale = originalScale;
+        currentGameObject.transform.localPosition = originalPos;
+        nextGameObject.SetActive(true);
     }
 }
