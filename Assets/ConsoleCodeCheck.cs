1c2acc6c4b3b9606bd54d0b00223a9434b860867
dav1nn <davinayyubov@gmail.com>
2025-03-18 17:51:55 +0000

    console code
    made it say continue- text gets replaced with text to fade in

--- Assets/ConsoleCodeCheck.cs
+++ Assets/ConsoleCodeCheck.cs
@@ -17,11 +17,19 @@ public class ConsoleCodeCheck : MonoBehaviour
     public Button nextButton;
     public TMP_Text consoleResponse;
     public List<GameObject> objectsToDisable;
+    public List<TMP_Text> noahConsoleMessages;
+    public TMP_InputField consoleInput;
+    public TMP_Text textToHide;
+    public TMP_Text textToFadeIn;
+    public float textFadeSpeed = 1f;
 
     private Color originalButtonColor;
     private string originalButtonText;
     private int errorCount = 0;
     private static HashSet<GameObject> disabledObjects = new HashSet<GameObject>();
+    private int messageIndex = 0;
+    private bool displayingText = false;
+    private bool codeAccepted = false;
 
     void Start()
     {
@@ -34,6 +42,7 @@ public class ConsoleCodeCheck : MonoBehaviour
         warningText.gameObject.SetActive(false);
         extraWarningText.gameObject.SetActive(false);
         finalWarningText.gameObject.SetActive(false);
+        textToFadeIn.gameObject.SetActive(false);
 
         foreach (GameObject obj in disabledObjects)
         {
@@ -41,6 +50,11 @@ public class ConsoleCodeCheck : MonoBehaviour
             {
                 obj.SetActive(false);
             }
+        }
+
+        foreach (TMP_Text message in noahConsoleMessages)
+        {
+            message.gameObject.SetActive(false);
         }
     }
 
@@ -53,38 +67,51 @@ public class ConsoleCodeCheck : MonoBehaviour
 
     public void OnNextPressed()
     {
-        if (input1.text == "88" && input2.text == "24" && input3.text == "1111")
+        if (!codeAccepted)
         {
-            input1.gameObject.SetActive(false);
-            input2.gameObject.SetActive(false);
-            input3.gameObject.SetActive(false);
-            consoleResponse.gameObject.SetActive(false);
-
-            foreach (GameObject obj in objectsToDisable)
+            if (input1.text == "88" && input2.text == "24" && input3.text == "1111")
             {
-                if (obj != null)
+                input1.gameObject.SetActive(false);
+                input2.gameObject.SetActive(false);
+                input3.gameObject.SetActive(false);
+                consoleResponse.gameObject.SetActive(false);
+                consoleInput.gameObject.SetActive(false);
+                codeAccepted = true;
+                nextButton.GetComponentInChildren<TMP_Text>().text = "CONTINUE";
+                textToHide.gameObject.SetActive(false);
+                StartCoroutine(FadeInText(textToFadeIn));
+
+                foreach (GameObject obj in objectsToDisable)
                 {
-                    obj.SetActive(false);
-                    disabledObjects.Add(obj);
+                    if (obj != null)
+                    {
+                        obj.SetActive(false);
+                        disabledObjects.Add(obj);
+                    }
                 }
+                DisplayNextMessage();
+            }
+            else
+            {
+                errorCount++;
+                if (errorCount == 3)
+                {
+                    StartCoroutine(FadeInText(warningText));
+                }
+                if (errorCount == 8)
+                {
+                    StartCoroutine(FadeInText(extraWarningText));
+                }
+                if (errorCount == 14)
+                {
+                    StartCoroutine(FadeInText(finalWarningText));
+                }
+                StartCoroutine(ShowErrorFeedback());
             }
         }
         else
         {
-            errorCount++;
-            if (errorCount == 3)
-            {
-                StartCoroutine(FadeInText(warningText));
-            }
-            if (errorCount == 8)
-            {
-                StartCoroutine(FadeInText(extraWarningText));
-            }
-            if (errorCount == 14)
-            {
-                StartCoroutine(FadeInText(finalWarningText));
-            }
-            StartCoroutine(ShowErrorFeedback());
+            DisplayNextMessage();
         }
     }
 
@@ -104,11 +131,10 @@ public class ConsoleCodeCheck : MonoBehaviour
         text.gameObject.SetActive(true);
         text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
         float elapsedTime = 0f;
-        float duration = 1f;
-        while (elapsedTime < duration)
+        while (elapsedTime < textFadeSpeed)
         {
             elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Clamp01(elapsedTime / duration);
+            float alpha = Mathf.Clamp01(elapsedTime / textFadeSpeed);
             text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
             yield return null;
         }
@@ -123,5 +149,35 @@ public class ConsoleCodeCheck : MonoBehaviour
                 obj.SetActive(false);
             }
         }
+    }
+
+    void DisplayNextMessage()
+    {
+        if (messageIndex < noahConsoleMessages.Count)
+        {
+            if (messageIndex > 0)
+            {
+                noahConsoleMessages[messageIndex - 1].gameObject.SetActive(false);
+            }
+
+            nextButton.interactable = false;
+            StartCoroutine(TypeMessage(noahConsoleMessages[messageIndex]));
+            messageIndex++;
+        }
+    }
+
+    IEnumerator TypeMessage(TMP_Text message)
+    {
+        displayingText = true;
+        message.gameObject.SetActive(true);
+        string fullText = message.text;
+        message.text = "";
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            message.text += fullText[i];
+            yield return new WaitForSeconds(0.05f);
+        }
+        displayingText = false;
+        nextButton.interactable = true;
     }
 }
