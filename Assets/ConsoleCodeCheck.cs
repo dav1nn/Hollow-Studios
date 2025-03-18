b63d664f6f01e2964814c16cb14f85c06e5cd651
dav1nn <davinayyubov@gmail.com>
2025-03-18 07:22:29 +0000

    console puzzle scripts
    codecheck similar to datecheck in day 1 puzzle
    
    console reset is being reworked

---
+++ Assets/ConsoleCodeCheck.cs
@@ -0,0 +1,96 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConsoleCodeCheck : MonoBehaviour
+{
+    public TMP_InputField input1;
+    public TMP_InputField input2;
+    public TMP_InputField input3;
+    public TMP_Text accessDeniedText;
+    public TMP_Text warningText;
+    public TMP_Text extraWarningText;
+    public TMP_Text finalWarningText;
+    public GameObject currentGameObject;
+    public Button nextButton;
+    public TMP_Text consoleResponse;
+
+    private Color originalButtonColor;
+    private string originalButtonText;
+    private int errorCount = 0;
+
+    void Start()
+    {
+        originalButtonColor = nextButton.GetComponent<Image>().color;
+        originalButtonText = nextButton.GetComponentInChildren<TMP_Text>().text;
+        nextButton.interactable = false;
+        input1.onValueChanged.AddListener(delegate { ValidateInputs(); });
+        input2.onValueChanged.AddListener(delegate { ValidateInputs(); });
+        input3.onValueChanged.AddListener(delegate { ValidateInputs(); });
+        warningText.gameObject.SetActive(false);
+        extraWarningText.gameObject.SetActive(false);
+        finalWarningText.gameObject.SetActive(false);
+    }
+
+    void ValidateInputs()
+    {
+        nextButton.interactable = !string.IsNullOrEmpty(input1.text) &&
+                                  !string.IsNullOrEmpty(input2.text) &&
+                                  !string.IsNullOrEmpty(input3.text);
+    }
+
+    public void OnNextPressed()
+    {
+        if (input1.text == "88" && input2.text == "24" && input3.text == "1111")
+        {
+            input1.gameObject.SetActive(false);
+            input2.gameObject.SetActive(false);
+            input3.gameObject.SetActive(false);
+            consoleResponse.gameObject.SetActive(false);
+        }
+        else
+        {
+            errorCount++;
+            if (errorCount == 3)
+            {
+                StartCoroutine(FadeInText(warningText));
+            }
+            if (errorCount == 8)
+            {
+                StartCoroutine(FadeInText(extraWarningText));
+            }
+            if (errorCount == 14)
+            {
+                StartCoroutine(FadeInText(finalWarningText));
+            }
+            StartCoroutine(ShowErrorFeedback());
+        }
+    }
+
+    IEnumerator ShowErrorFeedback()
+    {
+        nextButton.GetComponentInChildren<TMP_Text>().text = "ERROR";
+        nextButton.GetComponent<Image>().color = Color.red;
+        accessDeniedText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        nextButton.GetComponentInChildren<TMP_Text>().text = originalButtonText;
+        nextButton.GetComponent<Image>().color = originalButtonColor;
+        accessDeniedText.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeInText(TMP_Text text)
+    {
+        text.gameObject.SetActive(true);
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        float elapsedTime = 0f;
+        float duration = 1f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / duration);
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/ConsoleReset.cs
+++ Assets/Scripts/ConsoleReset.cs
@@ -1,65 +1,145 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ConsoleReset : MonoBehaviour
 {
-    [Header("Input / Output")]
     public TMP_InputField consoleInputField;
     public TextMeshProUGUI consoleOutputText;
-
-    [Header("Windows to Reset")]
+    public TextMeshProUGUI youOutputText;
     public Transform[] WindowsToReset;
 
-    private Vector3[] initialPositions;
-    private Quaternion[] initialRotations;
-    private Vector3[] initialScales;
+    private List<string> numberSequence = new List<string>();
+    private bool isTyping = false;
+
+    public float shakeIntensity = 2f;
+    public float shakeSpeed = 50f;
+    private TMP_TextInfo textInfo;
+    private List<int> shakyIndices = new List<int>();
+    private int maxLines = 10;
+    private int maxCharactersPerLine = 30;
 
     void Start()
     {
-        initialPositions = new Vector3[WindowsToReset.Length];
-        initialRotations = new Quaternion[WindowsToReset.Length];
-        initialScales = new Vector3[WindowsToReset.Length];
-
-        for (int i = 0; i < WindowsToReset.Length; i++)
-        {
-            initialPositions[i] = WindowsToReset[i].localPosition;
-            initialRotations[i] = WindowsToReset[i].localRotation;
-            initialScales[i] = WindowsToReset[i].localScale;
-        }
-
-        if (consoleOutputText != null)
-        {
-            consoleOutputText.text = "";
-        }
+        if (consoleOutputText != null) consoleOutputText.text = "";
+        if (youOutputText != null) youOutputText.text = "";
+        youOutputText.gameObject.SetActive(false);
     }
 
     public void OnInputSubmit()
     {
         string userInput = consoleInputField.text.Trim();
-
         if (!string.IsNullOrEmpty(userInput))
         {
             if (userInput.Equals("reset", System.StringComparison.OrdinalIgnoreCase))
             {
-                ResetWindows();
-                consoleOutputText.text = "All Programs Position Have Been Reset";
+                ResetConsole();
+            }
+            else if (userInput.Equals("YOU", System.StringComparison.OrdinalIgnoreCase))
+            {
+                if (!isTyping)
+                {
+                    consoleOutputText.text = "";
+                    StartCoroutine(TypeNumbersLoop());
+                }
             }
             else
             {
                 consoleOutputText.text = "Unknown command: " + userInput;
             }
         }
-
         consoleInputField.text = string.Empty;
     }
 
-    void ResetWindows()
+    void ResetConsole()
     {
-        for (int i = 0; i < WindowsToReset.Length; i++)
+        youOutputText.text = "";
+        youOutputText.gameObject.SetActive(false);
+        consoleOutputText.text = "Console has been reset.";
+        isTyping = false;
+        StopAllCoroutines();
+    }
+
+    void GenerateNumberSequence()
+    {
+        numberSequence.Clear();
+        List<string> baseNumbers = new List<string> { "88", "1111", "24" };
+        for (int i = 0; i < 80; i++)
         {
-            WindowsToReset[i].localPosition = initialPositions[i];
-            WindowsToReset[i].localRotation = initialRotations[i];
-            WindowsToReset[i].localScale = initialScales[i];
+            int randomValue = Random.Range(0, 2) == 0 ? Random.Range(10, 100) : Random.Range(1000, 10000);
+            numberSequence.Add(randomValue.ToString());
+            if (Random.Range(0, 5) == 0)
+            {
+                numberSequence.Add(baseNumbers[Random.Range(0, baseNumbers.Count)]);
+            }
+        }
+    }
+
+    IEnumerator TypeNumbersLoop()
+    {
+        isTyping = true;
+        youOutputText.gameObject.SetActive(true);
+        while (true)
+        {
+            GenerateNumberSequence();
+            youOutputText.text = "";
+            shakyIndices.Clear();
+            int counter = 0;
+            int lineCount = 0;
+            int currentLineLength = 0;
+            foreach (string num in numberSequence)
+            {
+                if (lineCount >= maxLines) break;
+                if (currentLineLength + num.Length + 1 > maxCharactersPerLine)
+                {
+                    youOutputText.text += "\n";
+                    lineCount++;
+                    currentLineLength = 0;
+                    if (lineCount >= maxLines) break;
+                }
+                youOutputText.text += num + " ";
+                currentLineLength += num.Length + 1;
+                if (num == "88" || num == "1111" || num == "24")
+                {
+                    shakyIndices.Add(youOutputText.text.Length - num.Length - 1);
+                }
+                counter++;
+                yield return new WaitForSeconds(0.02f);
+            }
+            StartCoroutine(ApplyShakyEffect());
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    IEnumerator ApplyShakyEffect()
+    {
+        while (true)
+        {
+            if (youOutputText == null) yield break;
+            youOutputText.ForceMeshUpdate();
+            textInfo = youOutputText.textInfo;
+            for (int i = 0; i < textInfo.characterCount; i++)
+            {
+                TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                if (!charInfo.isVisible) continue;
+                if (shakyIndices.Contains(charInfo.index))
+                {
+                    var meshInfo = textInfo.meshInfo[charInfo.materialReferenceIndex];
+                    Vector3[] vertices = meshInfo.vertices;
+                    int vertexIndex = charInfo.vertexIndex;
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Vector3 offset = new Vector3(
+                            Mathf.Sin(Time.time * shakeSpeed + j) * shakeIntensity,
+                            Mathf.Cos(Time.time * shakeSpeed + j) * shakeIntensity,
+                            0);
+                        vertices[vertexIndex + j] += offset;
+                    }
+                }
+            }
+            youOutputText.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+            yield return null;
         }
     }
 }
