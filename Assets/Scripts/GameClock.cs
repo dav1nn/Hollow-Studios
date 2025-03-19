a2dd2856a60ba8a1e34324ad97f4e69a5ab172f0
dav1nn <davinayyubov@gmail.com>
2025-03-19 03:43:23 +0000

    adjusted gameclock script
    gameclock script now does nothing but go up 1 minute each irl 10 seconds.
    game day 1/2/3 chat cleaned

--- Assets/Scripts/GameClock.cs
+++ Assets/Scripts/GameClock.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class GameClock : MonoBehaviour
 {
     public TMP_Text clockText;
-    private float gameTimeSeconds = 21f * 3600f;
-    private const float TIME_SCALE = 340f;
-    private bool transitionTriggered = false;
-    private bool isTimeStopped = false;
-    
+    private float gameTimeSeconds;
+    private float timer = 0f;
+
+    void Start()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "Day 1")
+            gameTimeSeconds = (21f * 3600f) + (32f * 60f);
+        else if (sceneName == "Day 2")
+            gameTimeSeconds = (13f * 3600f) + (16f * 60f);
+        else if (sceneName == "Day 3")
+            gameTimeSeconds = (19f * 3600f) + (52f * 60f);
+    }
+
     void Update()
     {
-        if (!isTimeStopped)
-            gameTimeSeconds += TIME_SCALE * Time.deltaTime;
-            
+        timer += Time.deltaTime;
+        if (timer >= 10f)
+        {
+            timer = 0f;
+            gameTimeSeconds += 60f;
+        }
+
         int totalSeconds = Mathf.FloorToInt(gameTimeSeconds);
         int hours24 = (totalSeconds / 3600) % 24;
         int minutes = (totalSeconds / 60) % 60;
@@ -24,38 +36,5 @@ public class GameClock : MonoBehaviour
             hours12 = 12;
         string ampm = hours24 < 12 ? "AM" : "PM";
         clockText.text = $"{hours12}:{minutes:00} {ampm}";
-        
-        if (hours24 >= 22 && !transitionTriggered)
-        {
-            transitionTriggered = true;
-            isTimeStopped = true;
-            gameTimeSeconds = 22f * 3600f;
-            StartCoroutine(LoadNextScene());
-        }
-    }
-    
-    private IEnumerator LoadNextScene()
-    {
-        int currentIndex = SceneManager.GetActiveScene().buildIndex;
-        int nextIndex = currentIndex + 1;
-        if (nextIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextIndex);
-        }
-        else
-        {
-            Debug.LogWarning("No next scene available in the build settings.");
-        }
-        yield break;
     }
 }
-
-
-
-
-
-
-
-
-
-
