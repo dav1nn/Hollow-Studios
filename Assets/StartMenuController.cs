5803b1c1a05dad9e1369db5fb8b2276a627743c6
dav1nn <davinayyubov@gmail.com>
2025-03-19 05:50:07 +0000

    added icons and buttons

--- Assets/StartMenuController.cs
+++ Assets/StartMenuController.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class StartMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject startMenuPanel;
+    public GameObject userProfileButton;
+
+    private bool isStartMenuOpen = false;
+
     void Start()
     {
-        
+        startMenuPanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isStartMenuOpen && Input.GetMouseButtonDown(0))
+        {
+            if (!IsClickInsideUI(startMenuPanel) && !IsClickInsideUI(userProfileButton))
+            {
+                ToggleStartMenu(false);
+            }
+        }
+    }
+
+    public void ToggleStartMenu(bool state)
+    {
+        isStartMenuOpen = state;
+        startMenuPanel.SetActive(state);
+    }
+
+    public void OnUserProfileClick()
+    {
+        ToggleStartMenu(!isStartMenuOpen);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private bool IsClickInsideUI(GameObject uiElement)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        foreach (var result in results)
+        {
+            if (result.gameObject == uiElement || result.gameObject.transform.IsChildOf(uiElement.transform))
+                return true;
+        }
+        return false;
     }
 }
