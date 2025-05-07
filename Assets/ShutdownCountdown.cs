da982d573ef23bcaccf96a50becd9a0856b74904
dav1nn <davinayyubov@gmail.com>
2025-05-07 18:15:54 +0100

    added end screen button
    sends u back to main menu and clears playrefs so u can replay the game

--- Assets/ShutdownCountdown.cs
+++ Assets/ShutdownCountdown.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ShutdownCountdown : MonoBehaviour
 {
     public TextMeshProUGUI countdownText;
+    public TextMeshProUGUI[] fadingTexts;
+    public TextMeshProUGUI restartPromptText;
+
+    private bool canRestart = false;
 
     private void Start()
     {
+        restartPromptText.gameObject.SetActive(false);
         StartCoroutine(StartShutdownSequence());
+    }
+
+    private void Update()
+    {
+        if (canRestart && Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene("Intro");
+        }
     }
 
     IEnumerator StartShutdownSequence()
@@ -23,7 +38,42 @@ public class ShutdownCountdown : MonoBehaviour
             yield return new WaitForSeconds(1f);
         }
 
-        Debug.Log("Game is closing...");
-        Application.Quit();
+        StartCoroutine(FadeOutTextsAndShowRestartPrompt());
+    }
+
+    IEnumerator FadeOutTextsAndShowRestartPrompt()
+    {
+        float duration = 1f;
+        float elapsed = 0f;
+
+        float[] originalAlphas = new float[fadingTexts.Length];
+        for (int i = 0; i < fadingTexts.Length; i++)
+        {
+            originalAlphas[i] = fadingTexts[i].color.a;
+        }
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = 1 - (elapsed / duration);
+
+            for (int i = 0; i < fadingTexts.Length; i++)
+            {
+                Color c = fadingTexts[i].color;
+                c.a = Mathf.Lerp(0, originalAlphas[i], t);
+                fadingTexts[i].color = c;
+            }
+
+            yield return null;
+        }
+
+        foreach (var text in fadingTexts)
+        {
+            text.gameObject.SetActive(false);
+        }
+
+        countdownText.gameObject.SetActive(false);
+        restartPromptText.gameObject.SetActive(true);
+        canRestart = true;
     }
 }
